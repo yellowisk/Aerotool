7cdb103688763fbd256b831d38e6fb93c3b7fb74
yellowisk <heitor.a@aluno.ifsp.edu.br>
2024-02-16 23:48:23 -0300

    feat: add register page

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,16 @@
+import { RegisterForm } from "@/components/register-form";
+import { User } from "lucide-react";
+
+export default function Register() {
+  return (
+    <div className="flex flex-1 flex-col space-y-4 h-screen items-center justify-center">
+      <div className="flex flex-col space-y-6 w-1/3 p-8 border-2 border-white/10 rounded">
+        <div className="flex flex-row space-x-2 items-center">
+          <User />
+          <h1 className="uppercase text-xl font-semibold">Registro</h1>
+        </div>
+        <RegisterForm />
+      </div>
+    </div>
+  );
+}
